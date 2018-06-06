8853746c0d6469180294cb47595f1e34a8f27e9a
Jarod Smith <jarodms@gmail.com>
2018-06-06 13:30:48 -0500

    add build scripts

---
+++ build-script.js
@@ -0,0 +1,14 @@
+const libName = 'ng6-lib';
+const fs = require('fs-extra');
+const concat = require('concat');    
+
+(async function build() {
+    try {
+        // TODO: Dynamically do this without version number
+        await fs.copy('dist/' + libName + '/' + libName + '-0.0.1.tgz', '../dist/' + libName + '-0.0.1.tgz')        
+      } catch (err) {
+        console.error(err)
+      }
+    console.info(libName + ' copied to lib folder')
+
+})()
